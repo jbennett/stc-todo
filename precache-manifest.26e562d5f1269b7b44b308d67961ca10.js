fc54e3d51c2666240c44c6fffa2c3210d14edbc5
Jonathan Bennett <jonathan@jbennett.me>
2020-02-26 21:31:39 -0500

    Updates

---
+++ precache-manifest.26e562d5f1269b7b44b308d67961ca10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "3de115a6ff5c4f0c7f9e9e8a1f47d585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "95f94037eac5ebb3d1f8",
+    "url": "/static/css/main.978bfbb7.chunk.css"
+  },
+  {
+    "revision": "95f94037eac5ebb3d1f8",
+    "url": "/static/js/main.95f94037.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
